36de3f804616bcc56caa804200edf8b307da4160
krassowski <krassowski.michal@gmail.com>
2021-01-03 20:16:24 +0000

    Split into jupyter_lsp and jupyterlab_lsp

---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+# flake8: noqa: F401
+from ._version import __version__
+from .manager import LanguageServerManager, lsp_message_listener
+from .serverextension import load_jupyter_server_extension
+from .specs.utils import NodeModuleSpec, ShellSpec
+from .types import (
+    KeyedLanguageServerSpecs,
+    LanguageServerManagerAPI,
+    LanguageServerSpec,
+)
+
+
+def _jupyter_server_extension_paths():
+    return [{"module": "jupyter_lsp"}]
+
+
+def _jupyter_labextension_paths():
+    return [
+        {
+            "src": "labextensions/@krassowski/jupyterlab-lsp",
+            "dest": "@krassowski/jupyterlab-lsp",
+        }
+    ]
